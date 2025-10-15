f6f81f3b42f6d1039894491dfe12e9b93f7ca309
biel <bimasi@jviladoms.cat>
2025-10-15 09:42:20 +0200

    Estructura corregida

--- backend/api/src/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const express = require('express');
-const connectDB = require('./config/DB');
-const app = express();
-app.use(express.json());
-connectDB();
-app.get('/', (req, res) => res.send('API Ecommerce en marxa 🚀'));
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor escoltant al port ${PORT}`));
